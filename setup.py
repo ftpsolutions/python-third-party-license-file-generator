705836383fd008a4558155c21c1a687d6a981335
Edward Beech <initialed85@gmail.com>
2020-08-18 11:36:30 +0800

    Merged in some PRs for a release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2020.7',
+    version='2020.7.18',
 
     description='The Python third_party_license_file_generator is aimed at distilling down the appropriate license for one or many pip "requirements" files into a single file; it supports Python2.7 and Python3.',
     long_description=long_description,
