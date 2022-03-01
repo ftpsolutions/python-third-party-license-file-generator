e54ffbdccfd7b5a5c18a9cb0f111548c48f9f80b
Edward Beech <initialed85@gmail.com>
2022-03-01 11:03:34 +0800

    Bumped version after recent PR

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2022.2.18",
+    version="2022.3.1",
     description='The Python third_party_license_file_generator is aimed at distilling down the appropriate license for one or many pip "requirements" files into a single file; it supports Python2.7 and Python3.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -62,7 +62,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["requests>=2.25", "PyYAML>=5.4"],
+    install_requires=[
+        "requests>=2.25",
+        "PyYAML>=5.4",
+    ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
