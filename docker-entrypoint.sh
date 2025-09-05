8a49d46a5c9dd3506abd1a85891b7e995aeb286a
Edward Beech <initialed85@gmail.com>
2025-09-05 20:24:21 +0800

    Fix up test.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,8 @@ python -u -m third_party_license_file_generator \
     -r /srv/python-third-party-license-file-generator/requirements.txt \
     -p "$(which python)" \
     --do-not-skip-not-required-packages \
-    --permit-gpl
+    --permit-gpl \
+	--skip-prefix twine
 
 if ! test -e THIRDPARTYLICENSES; then
     echo "error: couldn't find THIRDPARTYLICENSES- something has gone badly wrong"
